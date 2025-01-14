13d637d0bf0c7e53779303da76c61b5909eaf512
Bobby <bobur0218programmer@gmail.com>
2025-01-14 16:50:00 +0500

    update: cors

--- dist/config/cors.js
+++ dist/config/cors.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsConfig = void 0;
 exports.corsConfig = {
     credentials: true,
-    origin: "http://localhost:8080",
+    origin: "https://ppt-app-client.vercel.app",
     methods: ["GET", "POST"],
 };
--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,5 @@
 export const corsConfig = {
   credentials: true,
-  origin: "http://localhost:8080",
+  origin: "https://ppt-app-client.vercel.app",
   methods: ["GET", "POST"],
 };
